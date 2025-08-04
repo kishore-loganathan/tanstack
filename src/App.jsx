85be1f8ad9adaa66b57e4b9a4f3da21b92511c26
kishore-loganathan <kishoreloganathan773@gmail.com>
2025-08-04 13:36:51 +0530

    tried to show the text loading while loading the data using isfetching from usequery

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useQuery } from '@tanstack/react-query'
+
 const App = () => {
-  const{data}=useQuery({
+  const{data,isFetching}=useQuery({
     queryKey:["todo"],
     queryFn:getTodos,
   });
   return (
     <div>
-      <div>{JSON.stringify(data.slice(0,10))
+      <div>{isFetching?<h1>loading</h1>:JSON.stringify(data.slice(0,10))
         }</div>
     </div>
   )
 }
 const getTodos=async ()=>{
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   return await response.json();
 }
