2156677d4541c63e63e5affed04150154cfe55d3
kishore-loganathan <kishoreloganathan773@gmail.com>
2025-08-04 13:29:31 +0530

    tried to fetch json data from the endpoint by using the keyword data from the useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+const App = () => {
+  const{data}=useQuery({
+    queryKey:["todo"],
+    queryFn:getTodos,
+  });
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>{JSON.stringify(data.slice(0,10))
+        }</div>
+    </div>
   )
 }
-
+const getTodos=async ()=>{
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return await response.json();
+}
 export default App
